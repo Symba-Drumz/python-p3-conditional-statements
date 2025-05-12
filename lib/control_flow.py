cfeee6251eb23cc5e5aaf91385b967953519e43f
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-05-12 07:59:52 +0300

    bypass pass and write the necessary code to pass th e lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
-
+    if (username == "ADMIN" or username == "admin") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+    
+print(admin_login("ADMIN", "12345"))  # Access granted
+   
 def hows_the_weather(temperature):
-    # your code here
-    pass
+   if temperature < 40:
+       return "It's brisk out there!"
+   elif temperature >= 40 and temperature <= 60:
+        return "It's a little chilly out there!"
+   elif temperature > 85:
+        return "It's too dang hot out there!"
+   else:
+        return "It's perfect out there!"
+   
+print(hows_the_weather(35)) 
+print(hows_the_weather(55))
+print(hows_the_weather(99)) 
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
+print(fizzbuzz(15))  # FizzBuzz
+print(fizzbuzz(9))   # Fizz
+print(fizzbuzz(10))  # Buzz
+    
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+" or operation == "-" or operation == "*" or operation == "/":
+        return eval(f"{num1} {operation} {num2}")
+    else:
+        print("Invalid operation!")
+        return None
+    
+print(calculator("+", 5, 3))  # 8
+print(calculator("-", 5, 3))  # 2
+print(calculator("*", 5, 3))  # 15
+print(calculator("/", 5, 3))  # 1.6666666666666667
+print(calculator("**", 5, 3)) # Invalid operation and None
